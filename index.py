fbcbff64dacd606a62f245902d5c6607b64c8b89
Eduardo Raad <eraad@datil.co>
2018-11-21 17:07:16 -0500

    Add template support (#2)

--- index.py
+++ index.py
@@ -1,12 +1,21 @@
 import json
 import datetime
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+
+env = Environment(
+    loader=PackageLoader('ec-sri-registry', 'templates'),
+    autoescape=select_autoescape(['html'])
+)
 
 
 def handler(event, context):
+    template = env.get_template('home.html')
+    page = template.render()
     data = {
         'output': 'Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+            'body': page,
+            'headers': {'Content-Type': 'text/html'}}
