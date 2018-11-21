041c08826757146c37e9cf0b9db183b08be8571a
Eduardo Raad <eraad@datil.co>
2018-11-21 18:41:23 -0500

    Integrate Amazon Athena (#8)

--- index.py
+++ index.py
@@ -3,13 +3,28 @@
 import os
 import json
 import datetime
+from pyathenajdbc import connect
+
+
+def search_tax_id(tax_id):
+    conn = connect(s3_staging_dir='s3://ecuador-tax-registry/',
+                   schema_name='ec_tax_registry',
+                   region_name='us-west-2')
+
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+            SELECT numero_ruc, razon_social, estado_contribuyente, numero_establecimiento, estado_establecimiento FROM ec_tax_registry
+            WHERE numero_ruc = %(param)s
+            """, {'param': tax_id})
+            print(cursor.fetchall())
+    finally:
+        conn.close()
 
 
 def handler(event, context):
-    entity = {
-        "tax_id": "0992712554001",
-        "locations": []
-    }
+    entity = {"data": search_tax_id('0992712554001'),
+              "event": event}
     return {"statusCode": 200,
             "body": json.dumps(entity),
             "headers": {"Content-Type": "application/json"}}
